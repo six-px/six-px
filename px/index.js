f4660f0a90ffaa23458725b2a3c156b748dc02f9
aoove <10226093@qq.com>
2019-10-30 14:26:06 +0800

    input

--- px/index.js
+++ px/index.js
@@ -1,3 +1,4 @@
 export { default as button } from './button/index.css';
 export { default as fromLayout } from './formLayout/index.css';
 export { default as panel } from './panel/index.css';
+export { default as input } from './input/index.css';
